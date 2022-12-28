926a8ba6e3331d4f96be24ad2948f73a598f8e24
igorl1ma <43219164+igorl1ma@users.noreply.github.com>
2022-12-28 14:24:40 -0300

    Added maven framework Added Spring Boot framework

---
+++ src/test/java/com/stockFacilitation/StockFacilitationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockFacilitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockFacilitationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
