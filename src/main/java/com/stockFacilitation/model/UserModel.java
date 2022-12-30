010db68f5316c85dd28fc3de6cf7014c9569e976
igorl1ma <43219164+igorl1ma@users.noreply.github.com>
2022-12-30 16:07:38 -0300

    Created classes for User features (create, put and patch users)

---
+++ src/main/java/com/stockFacilitation/model/UserModel.java
@@ -0,0 +1,18 @@
+package com.stockFacilitation.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserModel {
+
+    private String userId;
+    private String username;
+    private String password;
+    private String name;
+    private Boolean accountNonExpired;
+    private Boolean accountNonLocked;
+    private Boolean credentialsNonExpired;
+    private Boolean enabled;
+}
